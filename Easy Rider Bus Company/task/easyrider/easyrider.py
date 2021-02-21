a9eeb4cd348e707e9a783d3cf3e65c3b83a6be9b
Val Bob <val.bob@example.com>
2021-02-21 14:28:54 +0200

    Check stop types

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -2,35 +2,22 @@
 import json
 
 
-def right_sequence(former, later):
-    t1 = [int(i) for i in former.split(':')]
-    t2 = [int(i) for i in later.split(':')]
-    if t2[0] > t1[0]:
-        return True
-    elif t2[0] == t1[0]:
-        if t2[1] > t1[1]:
-            return True
-    return False
-
-
 json_in = json.loads(input())
 
-print('Arrival time test:')
-same_bus = 0
-time_before = ''
-switch_flag = True  # To skip remained bus line stops if wrong time detected
+print('On demand stops test:')
+stop_name_type = {}
+wrong_stops = []
 ok = True  # Flag for no wrong times detected
 
 for row in json_in:
-    if row['bus_id'] == same_bus:
-        if switch_flag:
-            if not right_sequence(time_before, row['a_time']):
-                print(f'bus_id line {row["bus_id"]}: wrong time on station {row["stop_name"]}')
-                switch_flag = False
-                ok = False
+    if not stop_name_type.get(row['stop_name']):
+        stop_name_type[row['stop_name']] = set(row['stop_type'])
     else:
-        switch_flag = True
-    same_bus = row['bus_id']
-    time_before = row['a_time']
+        stop_name_type[row['stop_name']].add(row['stop_type'])
+        if 'O' in stop_name_type[row['stop_name']] and len(stop_name_type[row['stop_name']]) > 1:
+            wrong_stops.append(row['stop_name'])
+            ok = False
 if ok:
     print('OK')
+else:
+    print(f'Wrong stop type: {sorted(wrong_stops)}')
